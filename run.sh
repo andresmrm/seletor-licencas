fcc0522d7f5ed93534db73cff0840702afecb8f1
AndresMRM <andres@inventati.org>
2016-05-14 10:35:34 -0300

    observa mais arquivos

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
-ls css/main.css js/main.js modelos/* *.py | entr bash -c '. env/bin/activate; python tratar_dados.py; midori -e Reload' &
+ls css/main.css js/main.js modelos/* *.py licencas itens | entr bash -c '. env/bin/activate; python tratar_dados.py; midori -e Reload' &
 cd css; ls main.scss | entr sass --scss main.scss main.css
